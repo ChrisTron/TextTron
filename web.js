5b481dc3874b698194718cc441cf52db74df5d8a
Chris Fortier <cfortier@salesforce.com>
2013-10-13 15:25:57 -0700

    Telephone working

--- web.js
+++ web.js
@@ -1,12 +1,42 @@
 var express = require("express");
+var accountSid = proccess.env.twilSID
+		
+
+var authToken = process.env.twilToken
+var twilio = require('twilio')
+var client = new twilio.RestClient(accountSid, authToken)
+
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
 
 app.post('/sendtext', function(request, response) {
-	console.log('the cient sent:')
-	console.log(request.body);
-  response.send('text sent');
+	console.log('the client sent:');
+	// console.log ("the type of message is:" + typeof(request.body.message));
+	client.sms.messages.create({
+	to:'+1' +request.body.phone,
+	from:'+19252414697',
+	body: request.body.message
+	}, function(error, message) {
+		// The HTTP request to Twilio will run asynchronously. This callback
+		// function will be called when a response is received from Twilio
+		// The "error" variable will contain error information, if any.
+		// If the request was successful, this value will be "falsy"
+			console.log(error);
+		if (!error) {
+		// The second argument to the callback will contain the information
+		// sent back by Twilio for the request. In this case, it is the
+		// information about the text messsage you just sent:
+		console.log('Success! The SID for this SMS message is:');
+		console.log(message.sid);
+		 
+		console.log('Message sent on:');
+		console.log(message.dateCreated);
+		}
+		else {
+		console.log('Oops! There was an error.');
+		}
+	});
 });
 
 var port = process.env.PORT || 5000;
