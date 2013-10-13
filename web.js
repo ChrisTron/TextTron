2a38cdee1ad68ea8d9a989554c816e128881f36d
Chris Fortier <cfortier@salesforce.com>
2013-10-13 14:17:11 -0700

    client talking to server

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.use(express.bodyParser());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.post('/sendtext', function(request, response) {
+	console.log('the cient sent:')
+	console.log(request.body);
+  response.send('text sent');
 });
 
 var port = process.env.PORT || 5000;
+app.configure(function(){
+	app.use(express.static(__dirname + '/static'));
+});
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
